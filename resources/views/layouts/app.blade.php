8399eb4defe3e6c6225f79f4d9af7c7d2c2bef04
hamadpashua <102689093+hamadpashua@users.noreply.github.com>
2022-11-22 17:19:33 +0300

    first push

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+<body class="antialiased">
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')]
+@section('content')
+    <h1 class="text-3xl py-4 p-4 bg-blue-600 text-white font-bold underline">
+        Post index
+    </h1>
+@endsection
